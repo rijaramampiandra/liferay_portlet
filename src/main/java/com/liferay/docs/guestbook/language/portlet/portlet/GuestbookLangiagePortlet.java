4e523e0738dc8c0513a4355edaec9ec22762bd65
Rija RAMAMPIANDRA <rijaramampiandra@gmail.com>
2018-02-06 18:00:16 +0300

    <dev> Liferay portlet.

---
+++ src/main/java/com/liferay/docs/guestbook/language/portlet/portlet/GuestbookLangiagePortlet.java
@@ -0,0 +1,23 @@
+package com.liferay.docs.guestbook.language.portlet.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=guestbook-web-language Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class GuestbookLangiagePortlet extends MVCPortlet {
+}
---
+++ src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,6 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="guestbook-web-language.caption"/></b>
+	<b><liferay-ui:message key="caption"/></b>
+</p>
